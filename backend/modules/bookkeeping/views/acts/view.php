ce3b8a4b1b449602253cba0335b90a0c330247c7
Motuz Zhenya <e.motuz@webmart.by>
2015-11-04 10:11:29 +0300

    add pdfviewer

--- backend/modules/bookkeeping/views/acts/view.php
+++ backend/modules/bookkeeping/views/acts/view.php
@@ -71,6 +71,60 @@ $this->params['breadcrumbs'][] = $this->title;
                                 ],
                             ],
                         ]) ?>
+
+                    <!-- @todo вынести в oтдельный виджет --->
+                    <!-- In production, only one script (pdf.js) is necessary -->
+                    <!-- In production, change the content of PDFJS.workerSrc below -->
+                    <script src="<?php echo Yii::getAlias('@web/js/pdfjs/src/shared/util.js') ?>"></script>
+                    <script src="<?php echo Yii::getAlias('@web/js/pdfjs/src/display/api.js') ?>"></script>
+                    <script src="<?php echo Yii::getAlias('@web/js/pdfjs/src/display/metadata.js') ?>"></script>
+                    <script src="<?php echo Yii::getAlias('@web/js/pdfjs/src/display/canvas.js') ?>"></script>
+                    <script src="<?php echo Yii::getAlias('@web/js/pdfjs/src/display/webgl.js') ?>"></script>
+                    <script src="<?php echo Yii::getAlias('@web/js/pdfjs/src/display/pattern_helper.js') ?>"></script>
+                    <script src="<?php echo Yii::getAlias('@web/js/pdfjs/src/display/font_loader.js') ?>"></script>
+                    <script src="<?php echo Yii::getAlias('@web/js/pdfjs/src/display/annotation_helper.js') ?>"></script>
+                    <script>
+                        // Specify the main script used to create a new PDF.JS web worker.
+                        // In production, leave this undefined or change it to point to the
+                        // combined `pdf.worker.js` file.
+                        PDFJS.workerSrc = '<?php echo Yii::getAlias('@web/js/pdfjs/src/worker_loader.js') ?>';
+                        'use strict';
+                        //
+                        // Fetch the PDF document from the URL using promises
+                        //
+                        PDFJS.getDocument('<?php echo \yii\helpers\Url::to(['download-file','ask' => $model->ask])?>').then(function(pdf) {
+                            // Using promise to fetch the page
+                            pdf.getPage(1).then(function(page) {
+                                var scale = 1.5;
+                                var viewport = page.getViewport(scale);
+
+                                //
+                                // Prepare canvas using PDF page dimensions
+                                //
+                                var canvas = document.getElementById('the-canvas');
+                                var context = canvas.getContext('2d');
+                                canvas.height = viewport.height;
+                                canvas.width = viewport.width;
+
+                                //
+                                // Render PDF page into canvas context
+                                //
+                                var renderContext = {
+                                    canvasContext: context,
+                                    viewport: viewport
+                                };
+                                page.render(renderContext);
+                            });
+                        });
+                    </script>
+
+                    <div>
+                        <h3><?php echo Yii::t('app/book', 'Preview')?></h3>
+                        <div class="pdfPreview">
+                            <canvas id="the-canvas" style="border:1px solid black;"/>
+                        </div>
+                    </div>
+
                 </div>
             </div>
         </div>
