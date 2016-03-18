ad86878f83a028dbf1f3aa4ca045881798e2d914
Motuz Zhenya <e.motuz@webmart.by>
2016-03-18 12:30:45 +0300

    add manager rigts edit payments

--- backend/modules/bookkeeping/views/default/index.php
+++ backend/modules/bookkeeping/views/default/index.php
@@ -50,7 +50,8 @@ $this->params['breadcrumbs'][] = $this->title;
                                         $name = is_object($cuser = $model->cuser) ? $cuser->getInfo() : 'N/A';
                                         if(
                                             Yii::$app->user->can('adminRights') ||
-                                            Yii::$app->user->can('only_bookkeeper')
+                                            Yii::$app->user->can('only_bookkeeper') ||
+                                            Yii::$app->user->can('only_manager')
                                         )
                                             return Html::a($name,['update','id'=>$model->id],['class'=>'link-upd']);
                                         else
