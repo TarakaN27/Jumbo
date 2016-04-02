9a7f6ce06c10f11b31c472dffb1bb1072913fa23
Motuz Zhenya <motuzyayheni@gmail.com>
2016-04-02 09:55:34 +0300

    fix typo

--- common/messages/ru_RU/bookkeeping.php
+++ common/messages/ru_RU/bookkeeping.php
@@ -205,7 +205,7 @@ return [
     'Contract date' => 'Дата контракта',
     'Responsibility' => 'Ответственный',
     'Cuser for OP' => 'Погасить ОП другого контрагента',
-    'Attention. Unit will be enroll for payment request owner!' => 'Внимание! Юнит будеты будут зачислены владельцу запроса на платеж'
+    'Attention. Unit will be enroll for payment request owner!' => 'Внимание! Юниты будут зачислены владельцу запроса на платеж'
 
 
 
