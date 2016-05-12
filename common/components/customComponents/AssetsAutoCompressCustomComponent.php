6651957048456ad86cae6cccf91466321a4d9d30
Motuz Yauheni <e.motuz@webmartsoft.com>
2016-05-12 10:32:12 +0300

    add minify css and js

---
+++ common/components/customComponents/AssetsAutoCompressCustomComponent.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: zhenya
+ * Date: 12.5.16
+ * Time: 10.11
+ */
+
+namespace common\components\customComponents;
+
+
+use skeeks\yii2\assetsAuto\AssetsAutoCompressComponent;
+
+class AssetsAutoCompressCustomComponent extends AssetsAutoCompressComponent
+{
+
+        public 
+            $notUseWeb = false;
+    
+        
+
+
+}
