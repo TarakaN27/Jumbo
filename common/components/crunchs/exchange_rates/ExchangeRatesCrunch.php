5f4aebc6251e75eb2e570df9d1a8c4851af8eb12
Motuz Yauheni <e.motuz@webmartsoft.com>
2016-06-10 15:01:39 +0300

    add sleep

--- common/components/crunchs/exchange_rates/ExchangeRatesCrunch.php
+++ common/components/crunchs/exchange_rates/ExchangeRatesCrunch.php
@@ -69,6 +69,7 @@ class ExchangeRatesCrunch
 
         foreach ($arCurrency as $obCurr)
         {
+            sleep(2);
             if($obCurr->use_base || $obCurr->use_exchanger)
                 continue;
             if($obCurr->nbrb != 0) {
