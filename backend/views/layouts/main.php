e94f1e3d2be343a3d0c1cbf42484a86b8b19bf43
Motuz Yauheni <e.motuz@webmartsoft.com>
2016-06-28 10:30:47 +0300

    jira widget com back

--- backend/views/layouts/main.php
+++ backend/views/layouts/main.php
@@ -479,7 +479,7 @@ $menuItems[] = [
     <?php $this->endBody() ?>
     <?php if(!Yii::$app->user->isGuest):?>
         <!-- jira bug tracking -->
-        <!--script type="text/javascript" src="http://jira.webmartsoft.com/s/d41d8cd98f00b204e9800998ecf8427e/en_USdj7yrs-1988229788/6256/3/1.4.7/_/download/batch/com.atlassian.jira.collector.plugin.jira-issue-collector-plugin:issuecollector/com.atlassian.jira.collector.plugin.jira-issue-collector-plugin:issuecollector.js?collectorId=6d25f919"></script>
+        <script type="text/javascript" src="http://jira.webmartsoft.com/s/d41d8cd98f00b204e9800998ecf8427e/en_USdj7yrs-1988229788/6256/3/1.4.7/_/download/batch/com.atlassian.jira.collector.plugin.jira-issue-collector-plugin:issuecollector/com.atlassian.jira.collector.plugin.jira-issue-collector-plugin:issuecollector.js?collectorId=6d25f919"></script>
         <script type="text/javascript" src="http://jira.webmartsoft.com/s/d41d8cd98f00b204e9800998ecf8427e/en_USdj7yrs-1988229788/6256/3/1.4.7/_/download/batch/com.atlassian.jira.collector.plugin.jira-issue-collector-plugin:issuecollector/com.atlassian.jira.collector.plugin.jira-issue-collector-plugin:issuecollector.js?collectorId=2fdbcde1"></script>
         <script>
             window.ATL_JQ_PAGE_PROPS =  {
@@ -495,7 +495,7 @@ $menuItems[] = [
                     'YII_ENV' : '<?=YII_ENV;?>'
                 }
             };
-        </script-->
+        </script>
     <?php endif;?>
 
 </body>
