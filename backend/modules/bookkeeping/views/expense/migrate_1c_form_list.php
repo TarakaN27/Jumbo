6a9df41508abc68d1c6c71586c4c1bc53b9e31ad
a.ermalovich <a.ermalovich@webmartsoft.com>
2016-12-16 10:37:48 +0300

    фикс

--- backend/modules/bookkeeping/views/expense/migrate_1c_form_list.php
+++ backend/modules/bookkeeping/views/expense/migrate_1c_form_list.php
@@ -113,9 +113,15 @@ use common\components\helpers\CustomViewHelper;
                                 <?= $form->field($model, "[{$key}]description")->textarea(['rows' => 2])->label(false); ?>
                             </td>
                             <td width="15%">
-                                <?= $form->field($model, "[{$key}]cat_id")->dropDownList(
-                                    \common\models\ExpenseCategories::getExpenseCatMapWithoutParent(), [
-                                    'prompt' => Yii::t('app/book', 'BOOK_choose_expense_category')
+                                <?= $form->field($model, 'cat_id')->widget(\kartik\select2\Select2::className(),[
+                                    'data' => \common\models\ExpenseCategories::getExpenseCatTree(),
+                                    'initValueText' => Yii::t('app/book','BOOK_choose_expense_category'), // set the initial display text
+                                    'options' => [
+                                        'placeholder' => Yii::t('app/book','BOOK_choose_expense_category')
+                                    ],
+                                    'pluginOptions' => [
+                                        'allowClear' => true,
+                                    ]
                                 ])->label(false); ?>
                             </td>
                         </tr>
