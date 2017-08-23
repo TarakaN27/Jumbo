27f607ad688560eee7cfe324f3dd4f116f804122
e.yakovets <123456!>
2017-08-23 16:03:19 +0300

    Fix bill bug

--- backend/components/widgets/exchangeRates/views/exchange_rates.php
+++ backend/components/widgets/exchangeRates/views/exchange_rates.php
@@ -4,12 +4,6 @@ use yii\helpers\Url;
 ?>
 <?php Modal::begin([
 	'id' => 'exchange-rates-modal',
-	'header' => '<h2>'.Yii::t('app/common','Exchange rates').'</h2>'.\yii\jui\DatePicker::widget([
-            'id'=>'curr-date',
-            'dateFormat' => 'yyyy-MM-dd',
-            'value' => date('Y-m-d'),
-            'options' =>['class' => 'form-control'],
-        ]),
 	'size' => Modal::SIZE_DEFAULT,
 	'options' => [
 		'data-url' => Url::to(['/ajax-service/load-exchange-rates'])
