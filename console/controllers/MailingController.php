a73ccfb6e079dca029902da7ebe613bf89b997c0
Alex Erik <alex@webmart.by>
2017-12-05 12:18:32 +0300

    email newsletter

---
+++ console/controllers/MailingController.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: zhenya
+ * Date: 11.4.16
+ * Time: 12.16
+ */
+
+namespace console\controllers;
+
+use common\models\PaymentRequest;
+use console\components\AbstractConsoleController;
+use Yii;
+
+class MailingController extends AbstractConsoleController
+{
+    public function actionSendMailToCuser(){
+        $clients = PaymentRequest::find()->where(['legal_id'=>3])->groupBy(['cntr_id'])->all();
+        foreach($clients as $key=>$item) {
+            echo $key."\n";
+            if($item->cuser->requisites && $item->cuser->requisites->c_email && !strpos($item->cuser->requisites->c_email, "webmart.by")) {
+                if ($item->cuser) {
+                    $attach = Yii::getAlias("@common") . '/upload/Webmart_notice.pdf';
+                    Yii::$app->salesMailer->compose()
+                        ->setFrom([Yii::$app->params['salesEmail'] => Yii::$app->params['salesName']])//от кого уходит письмо
+                        ->setTo()
+                        ->setBcc(\Yii::$app->params['salesEmail'])//скрытая копия
+                        ->setSubject("УВЕДОМЛЕНИЕ Об изменении банковских реквизитов")//тема письма
+                        ->attach($attach)//прикрепляем акт к письму
+                        ->send();
+                }
+            }
+        }
+    }
+}
